f718d48742c7aac2428241a43b45e88072a0e9a2
Mayank kushwaha <mayankkushwaha226@gmail.com>
2024-11-03 23:49:05 +0530

    fix cookies access

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
-export const getAccessToken = () => cookies().get('accessToken')?.value;
+export const getAccessToken = async () => (await cookies()).get('accessToken')?.value;
 
 const AxiosInstance = axios.create({
     baseURL: process.env.DJANGO_SERVER_URL,
 });
 
-AxiosInstance.interceptors.request.use((config) => {
-    const token = getAccessToken();
+AxiosInstance.interceptors.request.use(async(config) => {
+    const token = await getAccessToken();
     if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
     }
